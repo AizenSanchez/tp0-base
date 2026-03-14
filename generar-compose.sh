3678933e9b24f29fb4dd47202ec4ed5170f06ccb
AizenSanchez <asancheza@fi.uba.ar>
2026-03-14 01:53:37 -0300

    fix: correct indentation in Docker Compose generation script

--- generar-compose.sh
+++ generar-compose.sh
@@ -14,16 +14,16 @@ services:
 
 for i in $(seq 1 $2); do
     echo "  client$i:
-        container_name: client$i
-        image: client:latest
-        entrypoint: /client
-        environment:
-            - CLI_ID=$i
-            - CLI_LOG_LEVEL=DEBUG
-        networks:
-            - testing_net
-        depends_on:
-            - server
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
 " >> $1
 done
 
@@ -32,5 +32,4 @@ echo "networks:
     ipam:
       driver: default
       config:
-        - subnet: 172.25.125.0/24
-" >> $1
+        - subnet: 172.25.125.0/24" >> $1
