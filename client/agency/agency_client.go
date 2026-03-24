bec2bcc327627769fafbecfec55c9df9466d8859
AizenSanchez <asancheza@fi.uba.ar>
2026-03-24 11:50:56 -0300

    feat: implement askForWinners method and add agency_id to protocol

--- client/agency/agency_client.go
+++ client/agency/agency_client.go
@@ -2,7 +2,9 @@ package agency
 
 import (
 	"fmt"
+	"strconv"
 
+	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/common"
 	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/communication"
 	"github.com/op/go-logging"
 )
@@ -14,7 +16,7 @@ const (
 var log = logging.MustGetLogger("log")
 
 type AgencyClient struct {
-	id            string
+	id            int
 	batch         BatchBuilder
 	serverAddress string
 	stopChannel   chan struct{}
@@ -25,8 +27,13 @@ func NewAgencyClient(id string, batchSize int, serverAddress string, stopChannel
 	if err != nil {
 		return AgencyClient{}, err
 	}
+
+	agencyId, err := strconv.Atoi(id)
+	if err != nil {
+		return AgencyClient{}, err
+	}
 	return AgencyClient{
-		id:            id,
+		id:            agencyId,
 		batch:         batch,
 		serverAddress: serverAddress,
 		stopChannel:   stopChannel,
@@ -52,7 +59,9 @@ func (agencyClient *AgencyClient) RegisterBets() error {
 		if err != nil {
 			return err
 		}
-		protocolFrame := communication.NewProtocolFrameRequestRegisterBets(batch)
+		bodyBytes := make([]byte, 0)
+		bodyBytes = append(bodyBytes, byte(agencyClient.id))
+		protocolFrame := communication.NewProtocolFrameRequestRegisterBets(bodyBytes)
 		err = serverConnection.SendMessage(protocolFrame)
 		if err != nil {
 			return err
@@ -74,4 +83,51 @@ func (agencyClient *AgencyClient) RegisterBets() error {
 		serverConnection.Close()
 	}
 	return nil
+}
+
+func (agencyClient *AgencyClient) AskForWinners() {
+	for {
+		serverConnection, err := communication.NewConnection(agencyClient.serverAddress, agencyClient.id)
+		if err != nil {
+			log.Errorf("action: ask_for_winners | result: fail | error: %v", err)
+			return
+		}
+		protocolFrame := communication.NewProtocolFrameRequestAskForWinners(agencyClient.id)
+		err = serverConnection.SendMessage(protocolFrame)
+		if err != nil {
+			log.Errorf("action: ask_for_winners | result: fail | error: %v", err)
+			return
+		}
+		protocolFrameResponse, err := serverConnection.ReceiveMessage()
+		if err != nil {
+			log.Errorf("action: ask_for_winners | result: fail | error: %v", err)
+			return
+		}
+		winners, results_ready, err := protocolFrameResponse.GetWinners()
+		if err != nil {
+			log.Errorf("action: ask_for_winners | result: fail | error: %v", err)
+			return
+		}
+		if !results_ready {
+			log.Infof("action: ask_for_winners | result: wait | agency_id: %v", agencyClient.id)
+			serverConnection.Close()
+			continue
+		}
+		log.Infof("action: ask_for_winners | result: success | agency_id: %v | winners_count: %v", agencyClient.id, len(winners))
+		serverConnection.Close()
+		agencyClient.showWinners(winners)
+		break
+	}
+}
+
+func (agencyClient *AgencyClient) showWinners(winners []common.ClientBet) {
+	for _, winner := range winners {
+		log.Infof("action: winner | result: success | client_name: %v | client_last_name: %v | client_dni: %v | client_birth_date: %v | bet_number: %v",
+			winner.GetClient().GetName(),
+			winner.GetClient().GetLastName(),
+			winner.GetClient().GetDni(),
+			winner.GetClient().GetBirthDate(),
+			winner.GetNumber(),
+		)
+	}
 }
